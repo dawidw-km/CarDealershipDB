0006537d3bf87bca5339ac733a106466143bd89d
dawidw-km <dawidwieclaw@hotmail.com>
2026-04-05 10:26:32 +0200

    initial commit

---
+++ cars/models.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+from django.db import models
+from django.core.validators import RegexValidator, MaxLengthValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
+
+
+vin_validator = RegexValidator(
+        regex=r'^[A-HJ-NPR-Z0-9]{17}$',
+        message='Invalid VIN format.'
+        )
+
+def year_validator(year):
+    current_year = datetime.now().year
+    if year < 1886:
+        raise ValidationError("Year of production cannot be earlier than 1886.")
+    if year > current_year:
+        raise ValidationError("Date of production cannot be in the future.")
+
+class Cars(models.Model):
+
+    STATUS_CHOICES = [
+        ('available', 'Available'),
+        ('reserved', "Reserved"),
+        ('sold', 'Sold'),
+    ]
+
+    brand = models.CharField(max_length=40)
+    model = models.CharField(max_length=40)
+    color = models.CharField(max_length=40)
+    vehicel_type = models.CharField(max_length=40)
+    year = models.PositiveIntegerField(
+            validators=[year_validator]
+            )
+
+    vin = models.CharField(max_length=17,
+                           unique=True,
+                           validators=[vin_validator]
+                           )
+
+    mileage = models.PositiveIntegerField(
+            validators=[MaxValueValidator(2000000)]
+            )
+
+    purchase_price = models.DecimalField(
+            max_digits=10,
+            decimal_places=2,
+            null=True,
+            blank=True
+            )
+    selling_price = models.DecimalField(
+            max_digits=10,
+            decimal_places=2,
+            null=True,
+            blank=True
+            )
+
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default="available"
+    )
+
+    description = models.TextField(
+            blank=True,
+            validators=[MaxLengthValidator(500)]
+            )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
---
+++ cars/tests.py
@@ -0,0 +1,96 @@
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
+from django.test import TestCase
+from .models import Cars
+
+class CarTestCase(TestCase):
+    def setUp(self):
+       self.car_a = Cars.objects.create(
+            brand='Toyota',
+            model='Corolla',
+            year=2004,
+            vin='123456789F2345678',
+            mileage=40000,
+            purchase_price=21500,
+        )
+    
+    def test_string_representation(self):
+        self.assertEqual(str(self.car_a), 'Toyota Corolla (2004)')
+
+    def test_car_status(self):
+        self.assertEqual(self.car_a.status, 'available')
+        
+    def test_valid_vin_number(self):
+        self.car_a.full_clean()
+    
+    def test_uniqueness_vin_error(self):
+        with self.assertRaises(IntegrityError):
+            Cars.objects.create(
+                    brand='Toyota',
+                    model='Wroom',
+                    year=2000,
+                    vin='123456789F2345678',
+                    mileage=2000,
+                    )
+
+
+    def test_invalid_vin_number(self):
+        car_2 = Cars(
+                brand='Mercedes',
+                model='Benz',
+                year=2000,
+                vin='QQQQQQQ123456789Q',
+                mileage=200000,
+                )
+
+        with self.assertRaises(ValidationError):
+            car_2.full_clean()
+
+    def test_description_too_long(self):
+        car_3 = Cars(
+                brand='BMW',
+                model='E200',
+                year=2010,
+                vin='12345678901234567',
+                mileage=253323,
+                description=('a' * 600)
+                )
+        
+        with self.assertRaises(ValidationError):
+            car_3.full_clean()
+
+    def test_year_too_early(self):
+        car_4 = Cars(
+                brand='Mercedes',
+                model='Benz',
+                year=1880,
+                vin='12345678901234567',
+                mileage=2000,
+                )
+
+        with self.assertRaises(ValidationError):
+            car_4.full_clean()
+
+    def test_year_too_late(self):
+        car_5 = Cars(
+                brand='Mercedes',
+                model='Benz',
+                year=5000,
+                vin='12345678901234567',
+                mileage=20000,
+                )
+        
+        with self.assertRaises(ValidationError):
+            car_5.full_clean()
+
+    def test_mileage_value_too_high(self):
+        car_6 = Cars(
+                brand='Mercedes',
+                model='Benz',
+                year=2021,
+                vin='12345678901234567',
+                mileage=20000000,
+                )
+
+        with self.assertRaises(ValidationError):
+            car_6.full_clean()
