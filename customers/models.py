75f3ef23d2daf93904e73521f7d214ee25272b9d
dawidw-km <dawidwieclaw@hotmail.com>
2026-04-05 17:49:18 +0200

    Created basic Customer model

--- customers/models.py
+++ customers/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Customers(models.Model):
     
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=100)
-
+    email = models.EmailField(unique=True)
+    phone_number = PhoneNumberField(region='PL', null=True, blank=True)
+    address = models.TextField()
+    date_of_birth = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
