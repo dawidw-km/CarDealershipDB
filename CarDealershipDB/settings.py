786f60843cf1a1f70bd86f9bc2a840b8b54d0463
dawidw-km <dawidwieclaw@hotmail.com>
2026-04-06 13:36:33 +0200

    Removed customers and replaced it with people for customer and employee

--- CarDealershipDB/settings.py
+++ CarDealershipDB/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cars',
-    'customers'
+    'person',
 ]
 
 MIDDLEWARE = [
--- person/models.py
+++ person/models.py
@@ -1,6 +1,6 @@
 from datetime import date
 from django.db import models
-from django.core.validators import RegexValidator, MinLengthValidator
+from django.core.validators import RegexValidator, MinLengthValidator, MinValueValidator
 from django.core.exceptions import ValidationError
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -18,8 +18,12 @@ def validate_birth_date(value):
     age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
     if age < 18:
         raise ValidationError("User has to be at least 18 years old.")
+    
+def validate_hire_date(value):
+    if value > date.today():
+        raise ValidationError("Hire date cannot be in the future.")
 
-class Customers(models.Model):
+class PersonBase(models.Model):
     
     first_name = models.CharField(
         max_length=40,
@@ -31,6 +35,33 @@ class Customers(models.Model):
           )
     email = models.EmailField(unique=True)
     phone_number = PhoneNumberField(region='PL')
+
+    class Meta:
+        abstract = True
+
+class Customers(PersonBase):
+
     address = models.TextField(validators=[MinLengthValidator(8)])
     date_of_birth = models.DateField(validators=[validate_birth_date])
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Employee(PersonBase):
+    
+    ROLE_CHOICES = [
+        ('worker', 'Worker'),
+        ('admin', 'Admin')
+    ]
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='worker'
+    )
+
+    hire_date = models.DateField(validators=[validate_hire_date])
+    salary = models.DecimalField(
+        max_digits=10,  
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+        )
