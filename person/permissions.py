309f4a5df99eacd40595164a95113a9ddff3e8ed
dawidw-km <dawidwieclaw@hotmail.com>
2026-05-09 11:53:34 +0200

    feat: implement custom DRF permissions

---
+++ person/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework.permissions import BasePermission
+
+
+
+class IsEmployeeAdmin(BasePermission):
+    """
+    Custom permission allowing only employee admins
+    (or Django superusers) to access certain views.
+    """
+    def has_permission(self, request, view):
+
+        if not request.user.is_authenticated:
+            return False
+        
+        if request.user.is_superuser:
+            return True
+
+        if not hasattr(request.user, 'employee_profile'):
+            return False
+
+        return request.user.employee_profile.role == 'admin'
+    
+class IsAnonymous(BasePermission):
+    """
+    Prevent logged-in users from creating additional accounts
+    """
+    def has_permission(self, request, view):
+        return not request.user.is_authenticated
