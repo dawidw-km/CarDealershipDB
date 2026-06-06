92044c7c11ec2e6d9301e9cbc49bab073f8c1043
dawidw-km <dawidwieclaw@hotmail.com>
2026-06-06 21:15:16 +0200

    feat: implement cars view for django templates

--- cars/views/template_views.py
+++ cars/views/template_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from cars.serializers import CarRegistrationSerializer
+from cars.models import Car, Status
 
 @login_required(login_url="customer-login-form")
 def customer_car_registration_view(request):
@@ -27,4 +28,21 @@ def customer_car_registration_view(request):
     return render(
         request,
         "cars/customer_car_registration.html",
+    )
+
+
+@login_required(login_url="customer-login-form")
+def customer_car_list_view(request):
+    """
+    Render a list of all cars available for purchase.
+    """
+    cars = Car.objects.filter(
+        status=Status.AVAILABLE,
+        is_deleted=False,
+        moderation_status=ModerationStatus.APPROVED
+        )
+    return render(
+        request,
+        "cars/customer_car_list.html",
+        {"cars": cars}
     )
