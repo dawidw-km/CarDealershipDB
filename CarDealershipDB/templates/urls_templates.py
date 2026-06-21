6c328382822f6d6bcfcdc2a2f60986d8dd39c24a
dawidw-km <dawidwieclaw@hotmail.com>
2026-06-21 07:21:36 +0200

    feat: add sale list tempalte, URLs, profile links and tests

--- CarDealershipDB/templates/urls_templates.py
+++ CarDealershipDB/templates/urls_templates.py
@@ -26,6 +26,9 @@ from cars.views.template_views import (
 )
 from sales.views.template_views import (
     customer_sale_registration_view,
+    buyer_sale_list_view,
+    owner_sale_list_view,
+    staff_sale_list_view,
 )
 
 urlpatterns = [
@@ -143,5 +146,20 @@ urlpatterns = [
         "customer/sale/register/<int:pk>/",
         customer_sale_registration_view,
         name="sale-car-registration-template"
+    ),
+    path(
+        "buyer/sales/",
+        buyer_sale_list_view,
+        name="buyer-sales-list-template"
+    ),
+    path(
+        "owner/sales/",
+        owner_sale_list_view,
+        name="owner-sales-list-template"
+    ),
+    path(
+        "staff/sales/",
+        staff_sale_list_view,
+        name="staff-sales-list-template"
     ),
 ]
--- sales/tests/test_templates.py
+++ sales/tests/test_templates.py
@@ -191,3 +191,256 @@ class SaleTemplateViewsTestCase(TestCase, TestHelpers):
         car.refresh_from_db()
         self.assertEqual(car.status, Status.AVAILABLE)
         self.assertEqual(car.buyer, None)
+
+    def test_buyer_can_view_his_transactions(self):
+        owner = self.create_customer("owner@example.com")
+        car = self.create_car(owner=owner)
+        employee = self.create_employee_worker("employee@example.com")
+        self.mark_car_as_approved(car, employee)
+        buyer = self.create_customer("buyer@example.com")
+
+        self.client.login(
+            username="buyer@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.post(
+            reverse("sale-car-registration-template",
+            args=[car.id]),
+            {"payment_method": Sale.PaymentMethod.CARD}
+        )
+        
+        self.assertRedirects(response, reverse("customer-profile"))
+
+        sale = Sale.objects.get(buyer=buyer, car=car)
+
+        response = self.client.get(reverse("buyer-sales-list-template"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "sales/sale_list.html")
+        self.assertContains(response, owner.first_name)
+        self.assertContains(response, buyer.first_name)
+        self.assertContains(response, "My Transactions")
+        self.assertContains(response, sale.transaction_number)
+
+    def test_owner_can_view_his_transactions(self):
+        owner = self.create_customer("owner@example.com")
+        car = self.create_car(owner=owner)
+        employee = self.create_employee_worker("employee@example.com")
+        self.mark_car_as_approved(car, employee)
+        buyer = self.create_customer("buyer@example.com")
+     
+
+        self.client.login(
+            username="buyer@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.post(
+            reverse("sale-car-registration-template",
+            args=[car.id]),
+            {"payment_method": Sale.PaymentMethod.CARD}
+        )
+        
+        self.assertRedirects(response, reverse("customer-profile"))
+
+        sale = Sale.objects.get(buyer=buyer, car=car)
+
+        self.client.login(
+            username="owner@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.get(reverse("owner-sales-list-template"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "sales/sale_list.html")
+        self.assertContains(response, owner.first_name)
+        self.assertContains(response, buyer.first_name)
+        self.assertContains(response, "My Transactions")
+        self.assertContains(response, sale.transaction_number)
+
+    def test_employee_can_view_transactions(self):
+        owner = self.create_customer("owner@example.com")
+        car = self.create_car(owner=owner)
+        employee = self.create_employee_worker("employee@example.com")
+        self.mark_car_as_approved(car, employee)
+        buyer = self.create_customer("buyer@example.com")
+
+        self.client.login(
+            username="buyer@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.post(
+            reverse("sale-car-registration-template",
+            args=[car.id]),
+            {"payment_method": Sale.PaymentMethod.CARD}
+        )
+        
+        self.assertRedirects(response, reverse("customer-profile"))
+        
+        sale = Sale.objects.get()
+
+        self.client.login(
+            username="employee@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.get(reverse("staff-sales-list-template"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "sales/sale_list.html")
+        self.assertContains(response, owner.first_name)
+        self.assertContains(response, buyer.first_name)
+        self.assertContains(response, "Transactions")
+        self.assertContains(response, sale.transaction_number)
+
+    def test_superuser_can_view_transactions(self):
+        owner = self.create_customer("owner@example.com")
+        car = self.create_car(owner=owner)
+        employee = self.create_employee_worker("employee@example.com")
+        self.mark_car_as_approved(car, employee)
+        buyer = self.create_customer("buyer@example.com")
+        superuser = self.create_superuser("superuser@example.com")
+
+        self.client.login(
+            username="buyer@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.post(
+            reverse("sale-car-registration-template",
+            args=[car.id]),
+            {"payment_method": Sale.PaymentMethod.CARD}
+        )
+        
+        self.assertRedirects(response, reverse("customer-profile"))
+        
+        sale = Sale.objects.get()
+
+        self.client.login(
+            username="superuser@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.get(reverse("staff-sales-list-template"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, sale.transaction_number)
+
+    def test_anonymous_user_cannot_access_buyer_sales_list_view(self):
+        login_url = reverse("login-form")
+        next_url = reverse("buyer-sales-list-template")
+
+        response = self.client.get(reverse("buyer-sales-list-template"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, login_url+"?next="+next_url)
+
+    def test_anonymous_user_cannot_access_owner_sales_list_view(self):
+        login_url = reverse("login-form")
+        next_url = reverse("owner-sales-list-template")
+
+        response = self.client.get(reverse("owner-sales-list-template"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, login_url+"?next="+next_url)
+
+    def test_anonymous_user_cannot_access_staff_sales_list_view(self):
+        login_url = reverse("login-form")
+        next_url = reverse("staff-sales-list-template")
+
+        response = self.client.get(reverse("staff-sales-list-template"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, login_url+"?next="+next_url)
+
+    def test_user_cannot_access_buyer_sales_list_view(self):
+        owner = self.create_customer("owner@example.com")
+        car = self.create_car(owner=owner)
+        employee = self.create_employee_worker("employee@example.com")
+        self.mark_car_as_approved(car, employee)
+        buyer = self.create_customer("buyer@example.com")
+        user = self.create_customer("user@example.com")
+
+        self.client.login(
+            username="buyer@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.post(
+            reverse("sale-car-registration-template",
+            args=[car.id]),
+            {"payment_method": Sale.PaymentMethod.CARD}
+        )
+
+        self.assertRedirects(response, reverse("customer-profile"))
+
+        self.client.login(
+            username="user@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.get(reverse("buyer-sales-list-template"))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_cannot_access_owner_sales_list_view(self):
+        owner = self.create_customer("owner@example.com")
+        car = self.create_car(owner=owner)
+        employee = self.create_employee_worker("employee@example.com")
+        self.mark_car_as_approved(car, employee)
+        buyer = self.create_customer("buyer@example.com")
+        user = self.create_customer("user@example.com")
+
+        self.client.login(
+            username="buyer@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.post(
+            reverse("sale-car-registration-template",
+            args=[car.id]),
+            {"payment_method": Sale.PaymentMethod.CARD}
+        )
+
+        self.client.login(
+            username="user@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.get(reverse("owner-sales-list-template"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("customer-profile"))
+        
+    def test_user_cannot_access_staff_sales_list_view(self):
+        owner = self.create_customer("owner@example.com")
+        car = self.create_car(owner=owner)
+        employee = self.create_employee_worker("employee@example.com")
+        self.mark_car_as_approved(car, employee)
+        buyer = self.create_customer("buyer@example.com")
+        user = self.create_customer("user@example.com")
+
+        self.client.login(
+            username="buyer@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.post(
+            reverse("sale-car-registration-template",
+            args=[car.id]),
+            {"payment_method": Sale.PaymentMethod.CARD}
+        )
+
+        self.assertRedirects(response, reverse("customer-profile"))
+
+        self.client.login(
+            username="user@example.com",
+            password="testpass123"
+        )
+
+        response = self.client.get(reverse("staff-sales-list-template"))
+
+        self.assertEqual(response.status_code, 403)
